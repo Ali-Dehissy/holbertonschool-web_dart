71afe0c03fb503656ecb6977c9f038cb75f46295
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2023-11-08 16:57:26 +0100

    fix

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -2,5 +2,5 @@ void main(List<String> args) {
   int.parse(args[0]) >= 80
       ? print("You Passed")
       : throw Exception(
-          "Uncaught Error: Assertion failed: "The score must be bigger or equal to 80");
+          'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
 }
