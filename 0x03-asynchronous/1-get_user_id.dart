46a63172e48378a835c60a7220e24000b0c543b9
Ali-Dehissy <97989740+Ali-Dehissy@users.noreply.github.com>
2023-11-27 15:20:30 +0100

    .

--- 0x03-asynchronous/1-get_user_id.dart
+++
@@ -1,8 +0,0 @@
-import ' 1-util.dart';
-import 'dart:convert';
-
-Future<String> getUserId() async {
-  String data = await fetchUserData();
-  Map dataMap = jsonDecode(data);
-  return dataMap['id'];
-}
